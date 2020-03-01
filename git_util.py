bd90d2a96e1c3c800ee3b6d8c8eda4ec022e9558
shraddha-koli <shraddhakoli.me@gmail.com>
2020-03-01 19:44:46 +0530

    Update git_util.py

--- git_util.py
+++ git_util.py
@@ -3,13 +3,15 @@ import sys
 import subprocess
 
 try:
-	os.mkdir('/home/shraddha/temp1')
+	os.mkdir('/home/shraddha/temp11')
 except OSError as e:
 	print (e)
 #to do install git
-os.chdir('/home/shraddha/temp1')
-os.system('sudo apt-get install git -y')
-os.system('git init --bare')
+os.chdir('/home/shraddha/temp11')
+ec= os.system('apt list --installed | grep git')
+if ec != 0:
+	os.system('sudo apt-get install git -y')
+os.system('git init')
 ec = os.system('git clone http://github.com/shraddha-koli/attribute-parser.git')
 print(ec)
 
